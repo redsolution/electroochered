3925c0ac2892bd6bbac6f34a0bf5427c5b7bc515
Evgeniy Gulitsyn <evgeniy.gulitsyn@redsolution.ru>
2013-06-11 15:48:00 +0600

    убрал модуль отчетов

--- sadiki/logger/urls.py
+++ sadiki/logger/urls.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import patterns, url
-from sadiki.logger.views import RequestionLogs, Reports, ReportsForType, \
-    ReportShow, RequestionLogsForAccount, RequestionLogsForOperator
+from sadiki.logger.views import RequestionLogs, RequestionLogsForAccount, RequestionLogsForOperator
 
 urlpatterns = patterns('',
     url(r'^request/(?P<requestion_id>\d{1,7})/$',
@@ -11,10 +10,4 @@ urlpatterns = patterns('',
     url(r'^operator/request/(?P<requestion_id>\d{1,7})/$',
         RequestionLogsForOperator.as_view(),
         name=u'requestion_logs_for_operator'),
-
-    url(r'^reports/$', Reports.as_view(), name=u'reports'),
-    url(r'^reports/by-type/(?P<report_type>\d{1,7})/$',
-        ReportsForType.as_view(), name=u'reports_for_type'),
-    url(r'^reports/(?P<report_id>\d{1,7})/$',
-        ReportShow.as_view(), name=u'show_report'),
 )
