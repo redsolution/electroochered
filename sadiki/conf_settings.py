bdacc229669c26bfaa65779cbb6308b396140e15
Dmitry Sobolev <dmitry.sobolev@redsolution.ru>
2013-10-23 17:08:07 +0600

    Изменил внутреннюю структуру файлов конфигурации

--- sadiki/conf_settings.py
+++ sadiki/conf_settings.py
@@ -6,9 +6,7 @@ from os.path import join, abspath, dirname
 import re
 
 config = RawConfigParser()
-config.read(join(dirname(dirname(abspath(__file__))), 'defaults.cfg'))
-config.read(join(dirname(dirname(abspath(__file__))),
-    'eturn-django-settings.ini'))
+config.read(join(dirname(dirname(abspath(__file__))), 'eturn-django-settings.ini'))
 
 
 # get from pyramid
@@ -43,11 +41,11 @@ def get_openlayers_urls(leaflet_url, leaflet_subdomains=None):
         return [url,]
 
 # [database]
-DATABASE_USER = config.get('database', 'DATABASE_USER')
-DATABASE_PASSWORD = config.get('database', 'DATABASE_PASSWORD')
-DATABASE_HOST = config.get('database', 'DATABASE_HOST')
-DATABASE_PORT = config.get('database', 'DATABASE_PORT')
-DATABASE_NAME = config.get('database', 'DATABASE_NAME')
+DATABASE_USER = config.get('custom options', 'DATABASE_USER')
+DATABASE_PASSWORD = config.get('custom options', 'DATABASE_PASSWORD')
+DATABASE_HOST = config.get('custom options', 'DATABASE_HOST')
+DATABASE_PORT = config.get('custom options', 'DATABASE_PORT')
+DATABASE_NAME = config.get('custom options', 'DATABASE_NAME')
 
 DATABASES = {
     'default': {
@@ -61,45 +59,45 @@ DATABASES = {
 }
 
 # [secrets]
-SECRET_KEY = config.get('secrets', 'SECRET_KEY')
+SECRET_KEY = config.get('custom options', 'SECRET_KEY')
 
 # [email]
-SERVER_EMAIL = config.get('email', 'SERVER_EMAIL')
-DEFAULT_FROM_EMAIL = config.get('email', 'DEFAULT_FROM_EMAIL')
-ADMINS = tuple(config.items('admin mail'))
-MANAGERS = tuple(config.items('stuff mail'))
+SERVER_EMAIL = config.get('custom options', 'SERVER_EMAIL')
+DEFAULT_FROM_EMAIL = config.get('custom options', 'DEFAULT_FROM_EMAIL')
+ADMINS = (config.get('custom options', 'WEBMASTER'), )
+MANAGERS = (config.get('custom options', 'STUFF'), )
 
 # [maps]
-MAP_CENTER = config.get('map', 'MAP_CENTER')
-MAP_ZOOM = config.getint('map', 'MAP_ZOOM')
-LEAFLET_TILES_URL = config.get('map', 'TILES_URL')
-LEAFLET_TILES_SUBDOMAINS = aslist(config.get('map', 'TILES_SUBDOMAINS'))
+MAP_CENTER = config.get('custom options', 'MAP_CENTER')
+MAP_ZOOM = config.getint('custom options', 'MAP_ZOOM')
+LEAFLET_TILES_URL = config.get('custom options', 'TILES_URL')
+LEAFLET_TILES_SUBDOMAINS = aslist(config.get('custom options', 'TILES_SUBDOMAINS'))
 OPENLAYERS_URLS = get_openlayers_urls(LEAFLET_TILES_URL, LEAFLET_TILES_SUBDOMAINS)
 
 # [options]
-MUNICIPALITY_OCATO = config.getint('options', 'MUNICIPALITY_OCATO')
-EMAIL_KEY_VALID = config.getint('options', 'EMAIL_KEY_VALID')
-MAX_CHILD_AGE = config.getint('options', 'MAX_CHILD_AGE')
+MUNICIPALITY_OCATO = config.getint('custom options', 'MUNICIPALITY_OCATO')
+EMAIL_KEY_VALID = config.getint('custom options', 'EMAIL_KEY_VALID')
+MAX_CHILD_AGE = config.getint('custom options', 'MAX_CHILD_AGE')
 # Например 17 Jul
-new_year_start_config = config.get('options', "NEW_YEAR_START").split()
+new_year_start_config = config.get('custom options', "NEW_YEAR_START").split()
 NEW_YEAR_START = datetime(
     year=1900, month=int(new_year_start_config[1]), day=int(new_year_start_config[0]))
-APPEAL_DAYS = config.getint('options', 'APPEAL_DAYS')
+APPEAL_DAYS = config.getint('custom options', 'APPEAL_DAYS')
 
-DESIRED_DATE = config.getint('options', 'DESIRED_DATE')
-DESIRED_SADIKS = config.getint('options', 'DESIRED_SADIKS')
-FACILITY_STORE = config.getint('options', 'FACILITY_STORE')
-TEMP_DISTRIBUTION = config.getint('options', 'TEMP_DISTRIBUTION')
-IMMEDIATELY_DISTRIBUTION = config.getint("options", "IMMEDIATELY_DISTRIBUTION")
-ETICKET = config.getint("options", "ETICKET")
+DESIRED_DATE = config.getint('custom options', 'DESIRED_DATE')
+DESIRED_SADIKS = config.getint('custom options', 'DESIRED_SADIKS')
+FACILITY_STORE = config.getint('custom options', 'FACILITY_STORE')
+TEMP_DISTRIBUTION = config.getint('custom options', 'TEMP_DISTRIBUTION')
+IMMEDIATELY_DISTRIBUTION = config.getint("custom options", "IMMEDIATELY_DISTRIBUTION")
+ETICKET = config.getint("custom options", "ETICKET")
 #переходы, которые особо отображаются в очереди
-SPECIAL_TRANSITIONS = [int(transition) for transition in config.get("options", "SPECIAL_TRANSITIONS").split(',')]
+SPECIAL_TRANSITIONS = [int(transition) for transition in config.get("custom options", "SPECIAL_TRANSITIONS").split(',')]
 #название типа документа, которое используется при импорте
-DEFAULT_IMPORT_DOCUMENT_NAME = config.get('options', 'DEFAULT_IMPORT_DOCUMENT_NAME')
+DEFAULT_IMPORT_DOCUMENT_NAME = config.get('custom options', 'DEFAULT_IMPORT_DOCUMENT_NAME')
 # название области, используется при импорте
-REGION_NAME = config.get('options', 'REGION_NAME').decode('utf-8')
+REGION_NAME = config.get('custom options', 'REGION_NAME').decode('utf-8')
 # маска для номера заявки(используется в модуле для jquery)
-REQUESTION_NUMBER_MASK = config.get('options', 'REQUESTION_NUMBER_MASK').decode('utf-8')
+REQUESTION_NUMBER_MASK = config.get('custom options', 'REQUESTION_NUMBER_MASK').decode('utf-8')
 # авторизация через ВКонтакте
-VK_APP_ID = config.get('options', 'VK_APP_ID')
-VK_API_SECRET = config.get('options', 'VK_API_SECRET')
+VK_APP_ID = config.get('custom options', 'VK_APP_ID')
+VK_API_SECRET = config.get('custom options', 'VK_API_SECRET')
