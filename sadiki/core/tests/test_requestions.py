b8b5295af3b2f7041bb500046ff4d0f5e936a856
Aleksey Osadchuk <aleksey.osadchuk@redsolution.ru>
2014-05-12 16:15:05 +0600

    more tests

--- sadiki/core/tests/test_requestions.py
+++ sadiki/core/tests/test_requestions.py
@@ -2,13 +2,16 @@
 import random
 import string
 import datetime
+from django.contrib.gis.geos import point
 from django.utils import unittest
 from django.conf import settings
 from django.contrib.auth.models import User, Permission
 from django.core import management
+from django.core.exceptions import ValidationError
 
 from sadiki.core.models import Requestion, Area, BenefitCategory, \
-    Profile
+    Profile, REQUESTION_TYPE_IMPORTED, REQUESTION_TYPE_CORRECTED, \
+    REQUESTION_TYPE_NORMAL
 
 
 def get_random_string(length, only_letters=False, only_digits=False):
@@ -62,7 +65,7 @@ def get_admission_date():
     return datetime.date(admission_date, 1, 1)
 
 
-def create_requestion():
+def create_requestion(**kwargs):
     birth_date = datetime.date.today()-datetime.timedelta(
         days=random.randint(0, settings.MAX_CHILD_AGE*12*30))
 
@@ -70,20 +73,51 @@ def create_requestion():
         'admission_date': get_admission_date(),
         'distribute_in_any_sadik': True,
         'birth_date': birth_date,
-        'profile': create_profile()
+        'profile': create_profile(),
+        'location_properties': 'челябинск',
+        'location': point.Point(random.choice([1, 2, 3, 4]), random.choice([1, 2, 3, 4])),
     }
-    r = Requestion.objects.create(**defaults)
-    r.areas.add(create_area())
-    return r
+    defaults.update(kwargs)
+    requestion = Requestion.objects.create(**defaults)
+    requestion.areas.add(create_area())
+    return requestion
 
 
-class SimpleTestCase(unittest.TestCase):
+class RequestionTestCase(unittest.TestCase):
     fixtures = ['sadiki/core/fixtures/test_initial.json', ]
 
-    def test_add(self):
-        self.assertEqual(2+2, 4)
+    @classmethod
+    def setUpClass(cls):
+        management.call_command('update_initial_data')
+
+    def setUp(self):
+        self.requestion = create_requestion(name='Ann')
 
     def test_requestion_creation(self):
-        management.call_command('update_initial_data')
-        # benefit_cat = create_benefit_category()
-        r1 = create_requestion()
+        self.assertEqual(self.requestion.name, 'Ann')
+        self.assertFalse(self.requestion.all_fields_filled())
+        self.requestion.sex = u'М'
+        self.assertTrue(self.requestion.all_fields_filled())
+        self.assertEqual(self.requestion.cast, REQUESTION_TYPE_NORMAL)
+
+    def test_location_not_verified(self):
+        self.requestion.cast = REQUESTION_TYPE_IMPORTED
+        self.assertTrue(self.requestion.needs_location_confirmation)
+        self.assertTrue(self.requestion.location_not_verified)
+
+        self.requestion.cast = REQUESTION_TYPE_CORRECTED
+        self.assertFalse(self.requestion.needs_location_confirmation)
+        self.assertFalse(self.requestion.location_not_verified)
+
+        self.requestion.cast = REQUESTION_TYPE_IMPORTED
+        self.requestion.location = None
+        self.assertTrue(self.requestion.location_not_verified)
+
+    def test_set_location(self):
+        self.requestion.set_location([1.5, 4.8])
+        self.assertEqual(self.requestion.location[0], 1.5)
+        self.assertEqual(self.requestion.location[1], 4.8)
+
+    def test_clean(self):
+        self.requestion.birth_date = datetime.date.today() + datetime.timedelta(days=1)
+        self.assertRaises(ValidationError, self.requestion.clean)
