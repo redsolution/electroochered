6feb330acac7c7030e4329e82f8a6ceea66c179e
Aleksey Osadchuk <aleksey.osadchuk@redsolution.ru>
2014-05-20 11:00:37 +0600

    показываем фильтр для анонимусов

--- sadiki/anonym/views.py
+++ sadiki/anonym/views.py
@@ -162,8 +162,6 @@ class Queue(RequirePermissionsMixin, ListView):
 
         # Обработать фильтры, если они есть
         self.queryset, form = self.process_filter_form(queryset, self.request.GET)
-        if self.request.user.is_anonymous() or not self.request.user.is_operator():
-            del form.fields['status']
         # Разбить на страницы
         page_size = self.paginate_by
         paginator, page, queryset, is_paginated = self.paginate_queryset(
