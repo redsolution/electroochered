18e20517de5a9b6e225fc00f8c0ba7f4216e7ad2
egor.lykov <egor.lykov@resolution.ru>
2014-07-10 17:53:10 +0600

    Удалил неиспользуемые файлы

--- sadiki/views.py
+++
@@ -1,5 +0,0 @@
-from django.views.generic.base import TemplateView
-from sadiki.core.permissions import RequirePermissionsMixin
-
-class OpenBoxMap(RequirePermissionsMixin, TemplateView):
-    template_name = 'openbox_map.html'
