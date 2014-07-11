f7fd7c0b246abb8a55ba8f49b31b94e018bd37ac
Aleksey Osadchuk <aleksey.osadchuk@redsolution.ru>
2014-07-11 15:51:40 +0600

    добавил вызовы api для получения данных о распределениях

--- sadiki/api/views.py
+++ sadiki/api/views.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-import json
 import calendar
 
 from django.utils import simplejson
-from django.core import serializers
 from django.http import HttpResponse
 
-from sadiki.core.models import Distribution
+from sadiki.core.models import Distribution, Requestion, Sadik
 
 
 def get_distributions(request):
@@ -19,11 +17,31 @@ def get_distribution(request, id):
     if len(distribution_qs) != 1:
         return HttpResponse(simplejson.dumps([0, ]), mimetype='text/json')
     dist = distribution_qs[0]
+    results = []
+    sadiks_ids = Requestion.objects.filter(
+        distributed_in_vacancy__distribution=dist).distinct().values_list(
+            'distributed_in_vacancy__sadik_group__sadik', flat=True)
+    for sadik in Sadik.objects.filter(
+            id__in=sadiks_ids).distinct().order_by('number'):
+        requestions = Requestion.objects.filter(
+            distributed_in_vacancy__distribution=dist,
+            distributed_in_vacancy__sadik_group__sadik=sadik).order_by(
+                '-birth_date').select_related('profile').select_related(
+                    'distributed_in_vacancy__sadik_group__age_group')
+        requestions.add_related_documents()
+        if requestions:
+            kg_dict = {'kindergtn': sadik.id, 'requestions': []}
+            for requestion in requestions:
+                kg_dict['requestions'].append({
+                    'requestion_number': requestion.requestion_number,
+                    'name': requestion.name,
+                    'birth_date': calendar.timegm(requestion.birth_date.timetuple())})
+            results.append(kg_dict)
     data = [{
         'id': dist.id,
         'start': calendar.timegm(dist.init_datetime.timetuple()),
         'end': calendar.timegm(dist.end_datetime.timetuple()),
         'year': dist.year.year,
-        'results': [],
+        'results': results,
     }]
     return HttpResponse(simplejson.dumps(data), mimetype='text/json')
