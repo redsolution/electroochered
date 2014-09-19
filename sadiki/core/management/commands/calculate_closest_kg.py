39b1ff4f5d2c32b5295930682679ee2e6b9f1a29
Aleksey Osadchuk <aleksey.osadchuk@redsolution.ru>
2014-09-19 11:07:31 +0600

    доработал команду расчета ближайшего ДОУ для заявки

--- sadiki/core/management/commands/calculate_closest_kg.py
+++ sadiki/core/management/commands/calculate_closest_kg.py
@@ -1,29 +1,51 @@
 # -*- coding: utf-8 -*-
 import math
+from optparse import make_option
 
 from django.core.management.base import BaseCommand
 
-from sadiki.core.models import Requestion, Area, Sadik, Distribution, \
-    STATUS_DECISION
+from sadiki.core.models import Requestion, Sadik
 
 
 class Command(BaseCommand):
-    help_text = '''Usage: manage.py send_statistic'''
+    help_text = '''Usage: manage.py calculate_closest_kg'''
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--only-empty',
+            action='store_true',
+            dest='only_empty',
+            default=False,
+            help='Calculate closest kindergarten only for empty requestions'),
+    )
 
     def handle(self, *args, **options):
         print "Calculating closest kindergartens to all requestions in queue"
-        requestions = Requestion.objects.all()
+        if options['only_empty']:
+            requestions = Requestion.objects.filter(
+                closest_kg=None).select_related('area')
+        else:
+            requestions = Requestion.objects.all().select_related('area')
+        n = 0
+        total = requestions.count()
         for requestion in requestions:
-            find_closest_kg(requestion)
+            if n % 100 == 0:
+                print "Calculating {} requestion from {} total".format(n, total)
+                print "Done {:.2f}%".format(n / float(total) * 100)
+            if requestion.location:
+                find_closest_kg(requestion)
+            n += 1
+        print "Done!"
 
 
 def find_closest_kg(requestion):
-    kgs = Sadik.objects.filter(area__in=requestion.areas.all())
+    kgs = Sadik.objects.filter(
+        area__in=requestion.areas.all()).select_related('address')
     closest = None
     for kg in kgs:
-        distance = measure_distance(requestion.location, kg.address.coords)
-        if not closest or closest['distance'] > distance:
-            closest = {'kg': kg, 'distance': distance}
+        if kg.address.coords:
+            distance = measure_distance(requestion.location, kg.address.coords)
+            if not closest or closest['distance'] > distance:
+                closest = {'kg': kg, 'distance': distance}
     requestion.closest_kg = closest['kg']
     requestion.save()
 
@@ -35,12 +57,12 @@ def measure_distance(coords1, coords2):
     lat2 = coords2[0]
     lon2 = coords2[1]
 
-    EARH_RADIUS = 6378.137
+    EARTH_RADIUS = 6378.137
     dLat = (lat2 - lat1) * math.pi / 180
     dLon = (lon2 - lon1) * math.pi / 180
     a = math.sin(dLat/2) * math.sin(dLat/2) + \
         math.cos(lat1 * math.pi / 180) * math.cos(lat2 * math.pi / 180) * \
         math.sin(dLon/2) * math.sin(dLon/2)
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    d = EARH_RADIUS * c
+    d = EARTH_RADIUS * c
     return d * 1000  # meters
