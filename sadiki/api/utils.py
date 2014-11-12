c2f72b2f0d01a1bf0b8c195095f7c7680b843177
Aleksey Osadchuk <aleksey.osadchuk@redsolution.ru>
2014-11-12 13:21:04 +0500

    вынес общие методы в отдельную функцию

--- sadiki/api/utils.py
+++ sadiki/api/utils.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 import gnupg
 
+from django.contrib.contenttypes.models import ContentType
+from django.core.urlresolvers import reverse
+
+from pysnippets import dttools
+from sadiki.core.models import Requestion, REQUESTION_IDENTITY, \
+    EvidienceDocument
+
 
 def sign_is_valid(data):
     gpg = gnupg.GPG()
@@ -16,3 +23,26 @@ def decrypt_data(data):
     gpg = gnupg.GPG()
     decrypted_data = gpg.decrypt(data)
     return decrypted_data
+
+
+def add_requestions_data(requestions, request):
+    """
+    Функция для сбора информации о заявках для дальнейшей конвертации в json.
+    Используется в API вызовах для экспорта зачисленных заявок в ЭС.
+    """
+    requestion_ct = ContentType.objects.get_for_model(Requestion)
+    req_list = []
+    for requestion in requestions:
+        birth_cert = EvidienceDocument.objects.filter(
+            template__destination=REQUESTION_IDENTITY,
+            content_type=requestion_ct, object_id=requestion.id)[0]
+        url = request.build_absolute_uri(reverse(
+            'requestion_logs', args=(requestion.id, )))
+        req_list.append({
+            'requestion_number': requestion.requestion_number,
+            'name': requestion.name,
+            'status': requestion.status,
+            'queue_profile_url': url,
+            'birth_date': dttools.date_to_stamp(requestion.birth_date),
+            'birth_cert': birth_cert.document_number})
+    return req_list
