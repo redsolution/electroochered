f3d46439faba58da3e0438a176a00c0e0de88951
Aleksey Osadchuk <aleksey.osadchuk@redsolution.ru>
2015-01-26 17:54:50 +0500

    добавил персональные данные в информацию о заявке

--- sadiki/api/utils.py
+++ sadiki/api/utils.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.contrib.contenttypes.models import ContentType
+from django.conf import settings
 from django.core.urlresolvers import reverse
 
 from pysnippets import dttools
@@ -20,7 +21,7 @@ def add_requestions_data(requestions, request):
             content_type=requestion_ct, object_id=requestion.id)[0]
         url = request.build_absolute_uri(reverse(
             'requestion_logs', args=(requestion.id, )))
-        req_list.append({
+        requestion_data = ({
             'requestion_number': requestion.requestion_number,
             'requestion_id': requestion.id,
             'distribution_datetime': dttools.datetime_to_stamp(
@@ -31,4 +32,28 @@ def add_requestions_data(requestions, request):
             'queue_profile_url': url,
             'birth_date': dttools.date_to_stamp(requestion.birth_date),
             'birth_cert': birth_cert.document_number})
+        if 'personal_data' in settings.INSTALLED_APPS:
+            requestion_data.update(get_personal_data(requestion))
+        req_list.append(requestion_data)
+
     return req_list
+
+
+def get_personal_data(requestion):
+    from personal_data.models import ChildPersData, UserPersData
+    pdata = {}
+    if ChildPersData.objects.filter(application=requestion).exists():
+        child_pdata = ChildPersData.objects.get(application=requestion)
+        pdata.update({
+            'second_name': child_pdata.second_name,
+            'last_name': child_pdata.last_name,
+        })
+    if UserPersData.objects.filter(profile=requestion.profile).exists():
+        user_pdata = UserPersData.objects.get(profile=requestion.profile)
+        pdata.update({
+            'parent_first_name': user_pdata.first_name,
+            'parent_second_name': user_pdata.second_name,
+            'parent_last_name': user_pdata.last_name,
+            'phone': user_pdata.phone,
+        })
+    return pdata
