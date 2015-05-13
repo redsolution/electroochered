649cb2d7675350391818dfbedcb72399a3607b13
sergey.vladimirov <sergey.vladimirov@redsolution.ru>
2015-05-13 16:38:49 +0500

    Заменил дублирующие поля в Profile на @property. Сами поля удалил

--- sadiki/core/models.py
+++ sadiki/core/models.py
@@ -763,9 +763,16 @@ class Profile(models.Model):
     # используется для указания принадлежности оператора к территориальной области
     area = models.ForeignKey('Area',
         verbose_name=u'Территориальная область к которой относится', null=True)
-    first_name = models.CharField(u'Имя', max_length=255, null=True)
+
+    @property
+    def first_name(self):
+        return self.user.first_name
+
+    @property
+    def last_name(self):
+        return self.user.last_name
+
     middle_name = models.CharField(u'Отчество', max_length=255, null=True)
-    last_name = models.CharField(u'Фамилия', max_length=255, null=True)
     email_verified = models.BooleanField(u'E-mail достоверный',
         default=False)
     phone_number = models.CharField(u'Телефон для связи', max_length=255,
