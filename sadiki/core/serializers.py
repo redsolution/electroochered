43944fd07c483dac12013f04c3585b5b92beff45
Aleksey Osadchuk <aleksey.osadchuk@redsolution.ru>
2015-05-19 15:50:46 +0500

    добавил serializer для возрастных групп

--- sadiki/core/serializers.py
+++ sadiki/core/serializers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from rest_framework import serializers
 
-from sadiki.core.models import Requestion, Sadik
+from sadiki.core.models import Requestion, Sadik, AgeGroup
 
 
 class RequestionGeoSerializer(serializers.ModelSerializer):
@@ -25,3 +25,14 @@ class SadikSerializer(serializers.ModelSerializer):
         model = Sadik
         fields = ('id', 'short_name', 'age_groups')
         read_only_fields = fields
+
+
+class AgeGroupSerializer(serializers.ModelSerializer):
+    max_birth_date = serializers.Field(source='max_birth_date')
+    min_birth_date = serializers.Field(source='min_birth_date')
+
+    class Meta:
+        model = AgeGroup
+        fields = ('id', 'name', 'short_name', 'max_birth_date',
+                  'min_birth_date')
+        read_only_fields = ('id', 'name', 'short_name')
