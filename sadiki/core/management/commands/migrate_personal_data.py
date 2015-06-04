fcceee33438186b79fdf2ed372ceb3821a0a48c4
sergey.vladimirov <sergey.vladimirov@redsolution.ru>
2015-06-04 16:12:34 +0500

    Дописал в команду сохранение логов

--- sadiki/core/management/commands/migrate_personal_data.py
+++ sadiki/core/management/commands/migrate_personal_data.py
@@ -4,6 +4,9 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
 from sadiki.core.models import Profile, Requestion
+from sadiki.core.workflow import CHANGE_USER_PERSONAL_DATA
+from sadiki.core.workflow import CHANGE_CHILD_PERSONAL_DATA
+from sadiki.logger.models import Logger
 from personal_data.models import ChildPersData, UserPersData
 
 
@@ -13,6 +16,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         all_users = User.objects.select_related().all()
         for user in all_users:
+            new_data = {}
             if hasattr(user, 'profile'):
                 profile = user.profile
                 if hasattr(profile, 'pdata'):
@@ -20,31 +24,50 @@ class Command(BaseCommand):
                     if not user.first_name and not user.last_name:
                         user.first_name = pdata.first_name
                         user.last_name = pdata.last_name
+                        new_data['first_name'] = pdata.first_name
+                        new_data['last_name'] = pdata.last_name
                         profile.middle_name = pdata.second_name
-                        user.save()
-                        profile.save()
-                        # TODO: создать лог записи name
+                        new_data['middle_name'] = pdata.second_name
                     profile.town = pdata.settlement
+                    new_data['town'] = pdata.settlement
                     profile.street = pdata.street
+                    new_data['street'] = pdata.street
                     profile.house = pdata.house
+                    new_data['house'] = pdata.house
                     profile.mobile_number = pdata.phone
-                    profile.save()
-                    # TODO: создать лог записи other
+                    new_data['phone'] = pdata.phone
                 if (not user.first_name and not user.last_name
                                 and profile.first_name):
                     user.first_name = profile.first_name
-                    user.save()
-                    # TODO: создать лог записи name from Profile
+                    new_data['first_name'] = profile.first_name
+                profile.save()
+            user.save()
+            if new_data:
+                Logger.objects.create_for_action(
+                    CHANGE_USER_PERSONAL_DATA,
+                    context_dict={'new_data': new_data},
+                    extra={'user': user},
+                    reason=u'Обновление до v1.9'
+                )
 
         all_child_personal_data = ChildPersData.objects.all()
         for pdata in all_child_personal_data:
+            new_data = {}
             requestion = pdata.application
             requestion.child_middle_name = pdata.second_name
+            new_data['child_middle_name'] = pdata.second_name
             requestion.child_last_name = pdata.last_name
+            new_data['child_last_name'] = pdata.last_name
             if not requestion.name:
                 requestion.name = pdata.first_name
+                new_data['child_first_name'] = pdata.first_name
             requestion.save()
-            # TODO: создать лог записи child name
+            Logger.objects.create_for_action(
+                CHANGE_CHILD_PERSONAL_DATA,
+                context_dict={'new_data': new_data},
+                extra={'user': requestion.profile.user},
+                reason=u'Обновление до v1.9'
+            )
 
 
 
