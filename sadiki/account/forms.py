d4b77d639bba1f6698e138657991315bd7364478
sergey.vladimirov <sergey.vladimirov@redsolution.ru>
2015-07-22 11:01:48 +0500

    Исправил ошибку с KeyError формы изменения заявки

--- sadiki/account/forms.py
+++ sadiki/account/forms.py
@@ -101,6 +101,8 @@ class ChangeRequestionForm(forms.ModelForm):
         super(ChangeRequestionForm, self).__init__(*args, **kwds)
 
     def clean(self, *args, **kwargs):
+        kinship_type = int(self.cleaned_data.get('kinship_type')
+                           or Requestion.REQUESTER_TYPE_OTHER)
         kinship_type = int(self.cleaned_data['kinship_type'])
         if kinship_type != Requestion.REQUESTER_TYPE_OTHER:
             self.cleaned_data['kinship'] = dict(
