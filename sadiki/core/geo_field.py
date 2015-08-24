3f0615d66228f4805da32f3e094d80d8683883d5
sergey.vladimirov <sergey.vladimirov@redsolution.ru>
2015-08-24 17:07:30 +0500

    Исправил текст ошибки, если не указаны координаты заявки

--- sadiki/core/geo_field.py
+++ sadiki/core/geo_field.py
@@ -4,4 +4,4 @@ from sadiki.core.models import Address
 from sadiki.core import admin as adm
 
 map_widget = admin.site._registry[Address].get_map_widget(Address._meta.get_field_by_name('coords')[0])
-location_errors = {'no_geom' : u"Не указана точка, определяющая местоположение",}
+location_errors = {'required' : u"Не указана точка, определяющая местоположение",}
