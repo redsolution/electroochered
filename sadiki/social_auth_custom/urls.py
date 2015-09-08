cde5a333c49d38fbe2dac949f2a3e6839490073e
sergey.vladimirov <sergey.vladimirov@redsolution.ru>
2015-09-08 11:46:54 +0500

    Corrected reversed URL names

--- sadiki/social_auth_custom/urls.py
+++ sadiki/social_auth_custom/urls.py
@@ -10,17 +10,17 @@ from social.apps.django_app.utils import psa
 from social.apps.django_app.views import auth, complete
 
 urlpatterns = patterns('',
-    url(r'^login/(?P<backend>[^/]+)/login/$', LoginAuth.as_view(), name='socialauth_login_begin'),
-    url(r'^login/(?P<backend>[^/]+)/registration/$', RegistrationAuth.as_view(), name='socialauth_registration_begin'),
-    url(r'^connect/(?P<backend>[^/]+)/$', login_required(auth), name='socialauth_connect'),
+    url(r'^login/(?P<backend>[^/]+)/login/$', LoginAuth.as_view(), name='login_begin'),
+    url(r'^login/(?P<backend>[^/]+)/registration/$', RegistrationAuth.as_view(), name='registration_begin'),
+    url(r'^connect/(?P<backend>[^/]+)/$', login_required(auth), name='connect'),
     url(r'^complete/(?P<backend>[^/]+)/$', complete,
         name='complete'),
     url(r'^complete/(?P<backend>[^/]+)/(?P<type>[^/]+)/$', custom_complete,
         name='complete'),
     url(r'^account_disconnect/(?P<backend>[^/]+)/(?P<association_id>[^/]+)/$',
-        AccountSocialAuthDisconnect.as_view(), name='account_social_auth_disconnect_individual'),
+        AccountSocialAuthDisconnect.as_view(), name='account_disconnect_individual'),
     url(r'^operator_disconnect/(?P<backend>[^/]+)/(?P<association_id>[^/]+)/$',
-        OperatorSocialAuthDisconnect.as_view(), name='operator_social_auth_disconnect_individual'),
+        OperatorSocialAuthDisconnect.as_view(), name='operator_disconnect_individual'),
 
     url(r'^account_social_data_update/$',
         AccountSocialAuthDataUpdate.as_view(), name='account_social_data_update'),
@@ -30,5 +30,4 @@ urlpatterns = patterns('',
         OperatorSocialAuthDataUpdate.as_view(), name='operator_social_data_update'),
     url(r'^operator_social_data_remove/(?P<user_id>\d{1,7})/$',
         OperatorSocialAuthDataRemove.as_view(), name='operator_social_data_remove'),
-    url(r'', include('social.apps.django_app.urls', namespace='social')),
 )
