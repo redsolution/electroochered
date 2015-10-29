90ff6b15b0f65c542cf72c61afcd429403cd88c0
sergey.vladimirov <sergey.vladimirov@redsolution.ru>
2015-10-29 11:35:57 +0500

    Add model ordering for correct loading of dump

--- sadiki/core/management/commands/depersonalize_data.py
+++ sadiki/core/management/commands/depersonalize_data.py
@@ -3,8 +3,10 @@ import time
 import logging
 import os
 import sys
+import itertools
 
 from django.core import management
+from django.apps import apps
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 from sadiki.core.utils import get_fixture_chunk_file_name as get_chunk_filename
@@ -59,10 +61,24 @@ class Command(management.base.BaseCommand):
                 if user_input == 'n':
                     print 'Exit...'
                     sys.exit()
+            model_list = list(
+                itertools.chain.from_iterable(
+                    app_config.get_models()
+                    for app_config in apps.get_app_configs()
+                    if app_config.models_module is not None)
+            )
+            sorted_model_list = sort_dependencies(model_list)
+            model_labels = [
+                '.'.join([model._meta.app_label, model._meta.model_name])
+                for model in sorted_model_list
+            ]
             logging.info(u"Начинаю экспорт, запускается dumpdata")
             start_time = time.time()
             management.call_command(
-                'dumpdata', '--format', 'djson', '--output', file_name
+                'dumpdata',
+                '--format', 'djson',
+                '--output', file_name,
+                *model_labels
             )
             logging.info(u"Экспорт завершен, время исполнения: {}:{}".format(
                 int(time.time() - start_time) / 60,
@@ -87,3 +103,75 @@ class Command(management.base.BaseCommand):
                 chunk_exists = os.path.exists(chunk_file_name)
                 chunk_number += 1
             print 'Dump from {} restored successfully'.format(file_name)
+
+
+def sort_dependencies(model_list):
+    """
+    Сортирует список моделей так, чтобы любая модель находилась в списке после
+    всех моделей, от которых имеются зависимости natural key, ForeignKey,
+    ManyToMany.
+    """
+    model_dependencies = []
+    for model in model_list:
+        # Add any explicitly defined dependencies
+        if hasattr(model, 'natural_key'):
+            deps = getattr(model.natural_key, 'dependencies', [])
+            if deps:
+                deps = [apps.get_model(dep) for dep in deps]
+        else:
+            deps = []
+        # Now add a dependency for any FK relation with a model that
+        # defines a natural key
+        for field in model._meta.fields:
+            if hasattr(field.rel, 'to'):
+                rel_model = field.rel.to
+                if rel_model != model:
+                    deps.append(rel_model)
+        # Also add a dependency for any simple M2M relation with a model
+        # that defines a natural key.  M2M relations with explicit through
+        # models don't count as dependencies.
+        for field in model._meta.many_to_many:
+            if field.rel.through._meta.auto_created:
+                rel_model = field.rel.to
+                if hasattr(rel_model, 'natural_key') and rel_model != model:
+                    deps.append(rel_model)
+        model_dependencies.append((model, deps))
+
+    model_dependencies.reverse()
+    # Now sort the models to ensure that dependencies are met. This
+    # is done by repeatedly iterating over the input list of models.
+    # If all the dependencies of a given model are in the final list,
+    # that model is promoted to the end of the final list. This process
+    # continues until the input list is empty, or we do a full iteration
+    # over the input models without promoting a model to the final list.
+    # If we do a full iteration without a promotion, that means there are
+    # circular dependencies in the list.
+    sorted_model_list = []
+    while model_dependencies:
+        skipped = []
+        changed = False
+        while model_dependencies:
+            model, deps = model_dependencies.pop()
+
+            # If all of the models in the dependency list are either already
+            # on the final model list, or not on the original serialization list,
+            # then we've found another model with all it's dependencies satisfied.
+            found = True
+            for candidate in ((d not in model_list or d in sorted_model_list)
+                              for d in deps):
+                if not candidate:
+                    found = False
+            if found:
+                sorted_model_list.append(model)
+                changed = True
+            else:
+                skipped.append((model, deps))
+        if not changed:
+            raise RuntimeError(
+                "Can't resolve dependencies for %s in serialized app list." %
+                ', '.join('%s.%s' % (model._meta.app_label, model._meta.object_name)
+                for model, deps in sorted(skipped, key=lambda obj: obj[0].__name__))
+            )
+        model_dependencies = skipped
+
+    return sorted_model_list
